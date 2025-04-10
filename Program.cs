e8ac42582e7bd132f4061a3ceaf256df7ea242f1
rahmaAlmamari <alr816125@gmail.com>
2025-04-10 10:55:57 +0400

    setup the start point of the project using StartSystem function and test if it work using DisplayWelcomeMessage function

--- Program.cs
+++ Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        //Main method ...
         static void Main(string[] args)
         {
-            
+            //calling StartSystem() to run the program ...
+            StartSystem();
+        }
+
+        //System Utilities & Final Flow Function (2)...
+        //1. StartSystem() ...
+        public static void StartSystem()
+        {
+            //calling DisplayWelcomeMessage() function ...
+            DisplayWelcomeMessage();
+        }
+        //2. ConfirmAction(string action) ...
+
+        //Startup & Navigation (4) ...
+        //1. DisplayWelcomeMessage() ...
+        public static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Codeline Airlines\nWe hope you have a pleasant time using our services (^0^)");
         }
     }
 }
